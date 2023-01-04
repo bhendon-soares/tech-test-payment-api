075258ef3a650da2c6d8c045bcd82997d9a00035
Bhendon Soares <bhendon.soares12@gmail.com>
2023-01-04 18:07:54 -0300

    Criação das migrations

---
+++ Migrations/20230104210614_CriacaoTabelaVenda.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace techtestpaymentapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTabelaVenda : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vendas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DescricaoProdutoVenda = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataVenda = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IdVendedor = table.Column<int>(type: "int", nullable: false),
+                    NomeVendedor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CPFVendedor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailVendedor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TelefoneVendedor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StatusVenda = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vendas");
+        }
+    }
+}
