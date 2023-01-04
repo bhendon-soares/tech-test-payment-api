671d56ece7034e0c4e6d0c6dfa475b09f4fdb336
Bhendon Soares <bhendon.soares12@gmail.com>
2023-01-04 17:58:00 -0300

    Criação do contexto para integração com o banco de dados

---
+++ Context/VendaContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tech_test_payment_api.Context
+{
+    public class VendaContext
+    {
+        
+    }
+}
